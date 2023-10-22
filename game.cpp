795bc3303f263eddeb9cd49c2735bb439653d0b4
bisht-arun <arunbisht402@gmail.com>
2023-10-22 18:31:21 +0530

    beautifyy

--- game.cpp
+++ game.cpp
@@ -5,112 +5,121 @@
 int tab[7][6];
 int choice, player;
 bool end = false;
-int a=0;
+int a = 0;
 
 void check(int x)
 {
-    if(tab[x-1][a]!=0 && a<6)
+    if (tab[x - 1][a] != 0 && a < 6)
     {
         a++;
         check(x);
-    }   else if (player==1 && a<6)
-            {
-            tab[x-1][a]=1;
-            a=0;
-            }
-        else if (player==2 && a<6)
-            {
-            tab[x-1][a]=2;
-            a=0;
-            }
-        else
-        {
+    }
+    else if (player == 1 && a < 6)
+    {
+        tab[x - 1][a] = 1;
+        a = 0;
+    }
+    else if (player == 2 && a < 6)
+    {
+        tab[x - 1][a] = 2;
+        a = 0;
+    }
+    else
+    {
         std::cout << "WRONG!" << std::endl;
-        a=0;
+        a = 0;
         player++;
-        }
+    }
 }
 
 void draw()
 {
     system("clear");
-    for(int i = 0; i<9; i++)
+    for (int i = 0; i < 9; i++)
     {
-        if(i<2)
+        if (i < 2)
         {
-        std::cout<<"-";
-        } else if(i>7)
+            std::cout << "-";
+        }
+        else if (i > 7)
         {
-        std::cout<<i-1<<"--"<<std::endl;
-        } else {
-        std::cout<<i-1<<"----";
+            std::cout << i - 1 << "--" << std::endl;
+        }
+        else
+        {
+            std::cout << i - 1 << "----";
         }
     }
-    for(int i = 0; i<6; i++)
+    for (int i = 0; i < 6; i++)
     {
-        for(int j = 0; j<7; j++)
+        for (int j = 0; j < 7; j++)
         {
-            if(tab[j][i]!=0)
+            if (tab[j][i] != 0)
             {
-                if(tab[j][i]==1)
+                if (tab[j][i] == 1)
                 {
-                std::cout<<"| X |";
-                }else std::cout<<"| O |";
+                    std::cout << "| X |";
+                }
+                else
+                    std::cout << "| O |";
             }
-            else std::cout<<"|   |";
-        } std::cout<<std::endl;
+            else
+                std::cout << "|   |";
+        }
+        std::cout << std::endl;
     }
-    for(int i = 0; i<35; i++)
+    for (int i = 0; i < 35; i++)
     {
-        std::cout<<"=";
-    } std::cout<<std::endl;
+        std::cout << "=";
+    }
+    std::cout << std::endl;
 }
 
 void win_check()
 {
-    for(int i = 0; i<6; i++)
+    for (int i = 0; i < 6; i++)
     {
-        for(int j = 0; j<7; j++)
+        for (int j = 0; j < 7; j++)
         {
-        if(tab[j][i]==1 && tab[j+1][i+1]==1 && tab[j+2][i+2]==1 && tab[j+3][i+3]==1)
+            if (tab[j][i] == 1 && tab[j + 1][i + 1] == 1 && tab[j + 2][i + 2] == 1 && tab[j + 3][i + 3] == 1)
             {
-            end = true;
-            std::cout << "\nPLAYER 1 WIN!" << std::endl;
+                end = true;
+                std::cout << "\nPLAYER 1 WIN!" << std::endl;
             }
-        if(tab[j][i]==1 && tab[j+1][i-1]==1 && tab[j+2][i-2]==1 && tab[j+3][i-3]==1)
+            if (tab[j][i] == 1 && tab[j + 1][i - 1] == 1 && tab[j + 2][i - 2] == 1 && tab[j + 3][i - 3] == 1)
             {
-            std::cout << "\nPLAYER 1 WIN!" << std::endl;
-            end = true;
+                std::cout << "\nPLAYER 1 WIN!" << std::endl;
+                end = true;
             }
-        if(tab[j][i]==2 && tab[j+1][i-1]==2 && tab[j+2][i-2]==2 && tab[j+3][i-3]==2)
+            if (tab[j][i] == 2 && tab[j + 1][i - 1] == 2 && tab[j + 2][i - 2] == 2 && tab[j + 3][i - 3] == 2)
             {
-            std::cout << "\nPLAYER 2 WIN!" << std::endl;
-            end=true;
+                std::cout << "\nPLAYER 2 WIN!" << std::endl;
+                end = true;
             }
-        else if(tab[j][i]==2 && tab[j-1][i-1]==2 && tab[j-2][i-2]==2 && tab[j-3][i-3]==2)
+            else if (tab[j][i] == 2 && tab[j - 1][i - 1] == 2 && tab[j - 2][i - 2] == 2 && tab[j - 3][i - 3] == 2)
             {
-            end = true;
-            std::cout << "\nPLAYER 2 WIN!" << std::endl;
+                end = true;
+                std::cout << "\nPLAYER 2 WIN!" << std::endl;
             }
-        else if(tab[j][i]==1 && tab[j][i-1]==1 && tab[j][i-2]==1 && tab[j][i-3]==1)
+            else if (tab[j][i] == 1 && tab[j][i - 1] == 1 && tab[j][i - 2] == 1 && tab[j][i - 3] == 1)
             {
-            std::cout << "\nPLAYER 1 WIN!" << std::endl;
-            end = true;
+                std::cout << "\nPLAYER 1 WIN!" << std::endl;
+                end = true;
             }
-        else if(tab[j][i]==1 && tab[j-1][i]==1 && tab[j-2][i]==1 && tab[j-3][i]==1)
+            else if (tab[j][i] == 1 && tab[j - 1][i] == 1 && tab[j - 2][i] == 1 && tab[j - 3][i] == 1)
             {
-            std::cout << "\nPLAYER 1 WIN!" << std::endl;
-            end = true;
+                std::cout << "\nPLAYER 1 WIN!" << std::endl;
+                end = true;
             }
-        else if(tab[j][i]==2 && tab[j][i-1]==2 && tab[j][i-2]==2 && tab[j][i-3]==2)
+            else if (tab[j][i] == 2 && tab[j][i - 1] == 2 && tab[j][i - 2] == 2 && tab[j][i - 3] == 2)
             {
-            std::cout << "\nPLAYER 2 WIN!" << std::endl;
-            end = true;
+                std::cout << "\nPLAYER 2 WIN!" << std::endl;
+                end = true;
             }
-        else if(tab[j][i]==2 && tab[j-1][i]==2 && tab[j-2][i]==2 && tab[j-3][i]==2)
+            else if (tab[j][i] == 2 && tab[j - 1][i] == 2 && tab[j - 2][i] == 2 && tab[j - 3][i] == 2)
             {
-            std::cout << "\nPLAYER 2 WIN!" << std::endl;
-            end = true;
+                std::cout << "\nPLAYER 2 WIN!" << std::endl;
+                end = true;
             }
         }
     }
@@ -118,11 +127,11 @@ void win_check()
 void p_choice()
 {
     player = 1;
-    while(end!=true)
+    while (end != true)
     {
-    std::cout << "PLAYER " << player << ": ";
-    std::cin >> choice;
-        if (choice>0 && choice<8)
+        std::cout << "PLAYER " << player << ": ";
+        std::cin >> choice;
+        if (choice > 0 && choice < 8)
         {
             check(choice);
             draw();
@@ -135,31 +144,30 @@ void p_choice()
                 player--;
             }
         }
-    else
+        else
         {
-    std::cout << "WRONG CHOICE!" << std::endl;
+            std::cout << "WRONG CHOICE!" << std::endl;
         }
-    win_check();
+        win_check();
     }
 }
-
 
 int main()
 {
     system("clear");
-    std::cout<<"WELCOME IN CONNECT 4"<<std::endl;
+    std::cout << "WELCOME IN CONNECT 4" << std::endl;
     sleep(1);
     draw();
     p_choice();
     return 0;
 }
 
-//Its a game called CONNECT 4
+// Its a game called CONNECT 4
 
 // the objective is
 
-//OBJECTIVE:
+// OBJECTIVE:
 
-//To be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally)
+// To be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally)
 
-//LETS PLAY IT!!!!!!
+// LETS PLAY IT!!!!!!
